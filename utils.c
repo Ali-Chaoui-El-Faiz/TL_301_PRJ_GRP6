de9d8866747a3896160b3efa916854ec04f1b96c
wacimH <wacim.hamdi-aissa@efrei.net>
2025-10-30 15:23:30 +0100

    partie 1 presque entierement complete

--- utils.c
+++ utils.c
@@ -3,6 +3,76 @@
 #include <string.h>
 
 #include "utils.h"
+// partie 1
+t_cell createcell(int som,float prob) {
+    t_cell cell;
+    cell.prob = prob;
+    cell.arr = som;
+    cell.next = NULL;
+    return cell;
+}
+t_list createemptylist() {
+    t_list newlist;
+    newlist.head = NULL;
+    return newlist;
+
+}
+void addcell(t_list *list, t_cell cell) {
+    list->head = &cell;
+}
+t_list_adj createlistadj(const int n) {
+    t_list_adj new;
+    new.n = n;
+    new.tab = (t_list*)malloc(n * sizeof(t_list));
+    for (int i = 0; i < n; i++) {
+        new.tab[i] = createemptylist();
+    }
+    return new;
+}
+void displaylist(t_list list) {
+    t_cell *cell;
+    cell = list.head;
+    printf("[head @] ->\n");
+    while (cell != NULL) {
+        printf(" (%d,%.2f) @->",cell->arr,cell->prob);
+        cell = cell->next;
+    }
+    printf("\n");
+}
+void displaylistadj (t_list_adj list) {
+    for (int i = 0; i < list.n; i++) {
+        printf("Liste pour le sommet %d:",i+1);
+        displaylist(list.tab[i]);
+    }
+}
+
+t_list_adj readGraph(const char *filename) {
+    FILE *file = fopen(filename, "rt"); // read-only, text
+    int nbvert, depart, arrivee;
+    float proba;
+    t_list_adj list_adj;
+    if (file==NULL)
+    {
+        perror("Could not open file for reading");
+        exit(EXIT_FAILURE);
+    }
+    // first line contains number of vertices
+    if (fscanf(file, "%d", &nbvert) != 1)
+    {
+        perror("Could not read number of vertices");
+        exit(EXIT_FAILURE);
+    }
+    list_adj = createemptylist(nbvert);
+    while (fscanf(file, "%d %d %f", &depart, &arrivee, &proba) == 3)
+    {
+        // on obtient, pour chaque ligne du fichier les valeurs
+        // depart, arrivee, et proba
+
+    }
+    fclose(file);
+    return list_adj;
+}
+
 
 static char *getID(int i)
 {
--- utils.h
+++ utils.h
@@ -1,5 +1,27 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+// partie 1
+typedef  struct s_cell {
+    int arr; // sommet d'arrive
+    float prob; // probabilite
+    struct s_cell *next; // prochaine cell
+} t_cell;
+
+typedef struct s_list {
+    t_cell *head;
+} t_list;
+typedef struct s_list_adj {
+    int n;
+    t_list *tab;
+} t_list_adj;
+t_cell createcell(int som,float prob);
+t_list createemptylist();
+void addcell(t_list *list, t_cell cell);
+void displaylistadj (t_list_adj list);
+void displaylist(t_list_adj list);
+t_list_adj createlistadj(int n);
+
+t_list_adj readGraph(const char *filename);
 
 #endif
